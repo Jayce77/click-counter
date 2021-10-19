aac7990c603ea6cf2255ceb94001888c9c1b19a3
Jayce77 <xjaycex@hotmail.com>
2021-10-19 17:21:17 -0400

    Added data atttribute tests and refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter() });
+const setUp = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, attr) => wrapper.find(`[data-test="${attr}"]`);
+
+test('renders without error', () => {
+  const wrapper = setUp();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+test('rennders counter display', () => {
+  const wrapper = setUp();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter display starts at 0', () => {
+  const wrapper = setUp();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('click button increments counter display', () => {
+
 });
